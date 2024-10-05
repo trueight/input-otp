00d62429426852100aa4fe32f3848a8c464d7b35
Hachi-R <drive8hachi@gmail.com>
2024-10-05 02:37:44 -0300

    lint:

--- src/lib/components/input.svelte
+++ src/lib/components/input.svelte
@@ -1,34 +1,43 @@
 <script lang="ts">
 	import { withPrevious } from '$lib/stores/previous.svelte';
+	import Monitor from './monitor.svelte';
 
 	const { maxLength = 6, defaultValue = '' } = $props();
 	const [newValue, previousValue] = withPrevious(defaultValue);
 
 	let inputElement: HTMLInputElement;
-	let selectionStart: number = $state(0);
-	let selectionEnd: number = $state(0);
+	const [newSelectionStart, previousSelectionStart] = withPrevious(0);
+	const [newSelectionEnd, previousSelectionEnd] = withPrevious(0);
+	const [newDirection, previousDirection] = withPrevious('none');
 
 	const handleSelectionChange = () => {
 		if (inputElement) {
-			selectionStart = inputElement.selectionStart ?? 0;
-			selectionEnd = inputElement.selectionEnd ?? 0;
+			$newSelectionStart = inputElement.selectionStart ?? 0;
+			$newSelectionEnd = inputElement.selectionEnd ?? 0;
 		}
-	}
+	};
 
 	$effect(() => {
-		if (inputElement &&typeof $previousValue === 'string' && $newValue.length < $previousValue.length) {
+		if (
+			inputElement &&
+			typeof $previousValue === 'string' &&
+			$newValue.length < $previousValue.length
+		) {
 			inputElement.dispatchEvent(new Event('selectionchange'));
 		}
 	});
 </script>
-
-current: {$newValue} - previous: {$previousValue}
-
-start: {selectionStart} - end: {selectionEnd}
 
 <input
 	onselectionchange={handleSelectionChange}
 	bind:this={inputElement}
 	bind:value={$newValue}
 	maxlength={maxLength}
+/>
+
+<Monitor
+	{maxLength}
+	newValue={$newValue}
+	newSelectionStart={$newSelectionStart}
+	newSelectionEnd={$newSelectionEnd}
 />
