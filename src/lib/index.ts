fe2da414cc31d44fd66dbef09d16a5272298e093
Hachi-R <drive8hachi@gmail.com>
2024-10-07 02:57:19 -0300

    feat: export components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
-// Reexport your entry components here
+import Input from './components/input.svelte';
+import Group from './components/group.svelte';
+import Slot from './components/slot.svelte';
+
+export { Input, Group, Slot };
