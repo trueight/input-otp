de128e837b97cbb3d4b12a599f29c275b9002227
Hachi-R <drive8hachi@gmail.com>
2024-10-09 01:28:52 -0300

    feat: export some pre made regexp

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import OTPInput from './components/input.svelte';
 import OTPGroup from './components/group.svelte';
 import OTPSlot from './components/slot.svelte';
+import { REGEXP_ONLY_CHARS, REGEXP_ONLY_DIGITS, REGEXP_ONLY_DIGITS_AND_CHARS } from './utils/regexp.js';
 
-export { OTPInput, OTPGroup, OTPSlot };
+export { OTPInput, OTPGroup, OTPSlot, REGEXP_ONLY_CHARS, REGEXP_ONLY_DIGITS, REGEXP_ONLY_DIGITS_AND_CHARS };
